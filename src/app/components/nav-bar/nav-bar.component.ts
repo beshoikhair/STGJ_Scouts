94c1b89607822b0fc47120499ec2d8a685fdaa34
beshoikhair <beshoikhair@gmail.com>
2023-09-03 17:44:17 -0400

    enhance  the navbar size and tab list

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -4,31 +4,76 @@ import { PageTabs } from 'src/app/Shared/interfaces';
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.less']
+  styleUrls: ['./nav-bar.component.less'],
 })
 export class NavBarComponent {
-
-  NavBarPages:PageTabs[] = [
+  NavBarPages: PageTabs[] = [
     {
       title: 'Home',
-      link:'*'
+      link: '*',
+      sub_titles: [],
     },
     {
       title: 'Gallery',
-      link:'*'
-    },   
-    {
-      title: 'Teams',
-      link:'*'
+      link: '*',
+      sub_titles: [],
     },
     {
-      title: 'Abous Us',
-      link:'*'
+      title: 'Teams',
+      link: '*',
+      sub_titles: [
+        {
+          title: 'Teams Overview',
+          link: '*',
+          sub_titles: [],
+        },
+        {
+          title: 'Louveteaux',
+          link: '*',
+          sub_titles: [],
+        },
+        {
+          title: 'Louvettes',
+          link: '*',
+          sub_titles: [],
+        },
+        {
+          title: 'Aventuriers',
+          link: '*',
+          sub_titles: [],
+        },
+        {
+          title: 'Pioneers',
+          link: '*',
+          sub_titles: [],
+        },
+        {
+          title: 'Leaders',
+          link: '*',
+          sub_titles: [],
+        },
+      ],
+    },
+    {
+      title: 'About Us',
+      link: '*',
+      sub_titles: [
+        {
+          title: 'STGJ Scouts',
+          link: '*',
+          sub_titles: [],
+        },
+        {
+          title: 'Baden Powell',
+          link: '*',
+          sub_titles: [],
+        },
+      ],
     },
     {
       title: 'Contact Us',
-      link:'*'
+      link: '*',
+      sub_titles: [],
     },
-  ]
-
+  ];
 }
