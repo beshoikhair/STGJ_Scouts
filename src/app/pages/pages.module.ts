8fcfc0c16f89419811c945bf3f434ab57a71a7ca
beshoikhair <beshoikhair@gmail.com>
2023-09-17 02:01:09 -0400

    Created teams_overview_page

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ComponentsModule } from '../components/components.module';
 import { HomePageComponent } from './home-page/home-page.component';
+import { TeamsOverviewPageComponent } from './teams-overview-page/teams-overview-page.component';
 
 @NgModule({
-  declarations: [HomePageComponent],
+  declarations: [HomePageComponent, TeamsOverviewPageComponent],
   imports: [CommonModule, ComponentsModule],
-  exports: [HomePageComponent],
+  exports: [HomePageComponent, TeamsOverviewPageComponent],
 })
 export class PagesModule {}
