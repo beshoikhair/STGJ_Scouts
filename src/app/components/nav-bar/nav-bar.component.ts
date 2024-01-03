ba70d29eb65befb87a01d70121e9f41ede5dc411
beshoikhair <beshoikhair@gmail.com>
2024-01-03 03:07:31 -0500

    solve contact-us and scrolling issue

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { PageTabs } from 'src/app/Shared/interfaces';
 
 @Component({
@@ -7,6 +8,7 @@ import { PageTabs } from 'src/app/Shared/interfaces';
   styleUrls: ['./nav-bar.component.less'],
 })
 export class NavBarComponent {
+  constructor(private router: Router) {}
   NavBarPages: PageTabs[] = [
     {
       title: 'Home',
@@ -72,8 +74,13 @@ export class NavBarComponent {
     },
     {
       title: 'Contact Us',
-      link: window.location.href + '#footer',
+      link: 'footer',
       sub_titles: [],
     },
   ];
+
+  getCurrentUrl(): string {
+    console.log(this.router.url);
+    return this.router.url;
+  }
 }
